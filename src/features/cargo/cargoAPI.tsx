046e4310246c3c2562b9ae03eeb2adf3e8939ce5
Elijah Kimani <elijahkimani1293@gmail.com>
2025-03-14 21:37:01 +0300

    first commit

--- src/features/cargo/cargoAPI.tsx
+++ src/features/cargo/cargoAPI.tsx
@@ -1,68 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-// const API_URL = "https://cargoapi-bqs3.onrender.com/";
-
-// export const cargoApi = createApi({
-//   reducerPath: "cargoApi",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: API_URL,
-//     prepareHeaders: (headers) => {
-//       const token = localStorage.getItem("token");
-//       console.log("Auth Token:", token);
-
-//       if (token) {
-//         headers.set("Authorization", `Bearer ${token}`);
-//       } else {
-//         console.warn("No token found in localStorage!");
-//       }
-//       return headers;
-//     },
-//     credentials: "include",
-//   }),
-//   endpoints: (builder) => ({
-//     getAllCargo: builder.query({
-//       query: () => "/cargo",
-//     }),
-//     getCargoById: builder.query({
-//       query: (id) => `/cargo/${id}`,
-//     }),
-//     createCargo: builder.mutation({
-//       query: (cargoData) => ({
-//         url: "/cargo",
-//         method: "POST",
-//         body: cargoData,
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }),
-//     }),
-//     updateCargo: builder.mutation({
-//       query: ({ id, cargoData }) => ({
-//         url: `/cargo/${id}`,
-//         method: "PUT",
-//         body: cargoData,
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }),
-//     }),
-//     deleteCargo: builder.mutation({
-//       query: (id) => ({
-//         url: `/cargo/${id}`,
-//         method: "DELETE",
-//       }),
-//     }),
-//   }),
-// });
-
-// export const {
-//   useGetAllCargoQuery,
-//   useGetCargoByIdQuery,
-//   useCreateCargoMutation,
-//   useUpdateCargoMutation,
-//   useDeleteCargoMutation,
-// } = cargoApi;
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_URL = "https://cargoapi-bqs3.onrender.com/";
@@ -82,7 +17,7 @@ export const cargoApi = createApi({
       }
       return headers;
     },
-    credentials: "include", // Include credentials for cookies or secure sessions
+    credentials: "include", // Ensures cookies and tokens are sent
   }),
   endpoints: (builder) => ({
     getAllCargo: builder.query({
